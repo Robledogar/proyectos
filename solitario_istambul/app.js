8ca66039aebdea70f2571568bc574481bebb1280
Robledogar <45006680+Robledogar@users.noreply.github.com>
2023-04-04 21:20:03 +0200

    Funciones añadidas

--- solitario_istambul/app.js
+++ solitario_istambul/app.js
@@ -7,19 +7,63 @@ const contenedor = document.querySelector('#contenedor');
 document.addEventListener("DOMContentLoaded", inicio);
 
 
-
+// ARRAY
+const arrayCartas = [
+    '<img class="carta" src="images/1.jpg"',
+    '<img class="carta" src="images/1.jpg"',
+    '<img class="carta" src="images/2.jpg"',
+    '<img class="carta" src="images/3.jpg"',
+    '<img class="carta" src="images/4.jpg"',
+    '<img class="carta" src="images/5.jpg"',
+    '<img class="carta" src="images/5.jpg"',
+    '<img class="carta" src="images/6.jpg"',
+    '<img class="carta" src="images/6.jpg"',
+    '<img class="carta" src="images/8.jpg"',
+    '<img class="carta" src="images/8.jpg"',
+    '<img class="carta" src="images/9.jpg"',
+    '<img class="carta" src="images/9.jpg"',
+    '<img class="carta" src="images/10.jpg"',
+    '<img class="carta" src="images/10.jpg"',
+    '<img class="carta" src="images/11.jpg"',
+    '<img class="carta" src="images/11.jpg"',
+    '<img class="carta" src="images/12.jpg"',
+    '<img class="carta" src="images/12.jpg"',
+    '<img class="carta" src="images/13.jpg"',
+    '<img class="carta" src="images/13.jpg"',
+    '<img class="carta" src="images/14.jpg"',
+    '<img class="carta" src="images/15.jpg"',
+    '<img class="carta" src="images/16.jpg"',
+    '<img class="carta" src="images/16.jpg"',
+    '<img class="carta" src="images/18.jpg"',
+    '<img class="carta" src="images/18.jpg"',
+    
+];
 
 
 // FUNCIONES 
 function inicio() {
     contenedor.innerHTML = `<h1>ISTAMBUL</h1>
                            <h2>MODO SOLITARIO</h2>
-                           <button id="botonComenzar">COMENZAR</button>`;
+                           <button class="boton" id="botonComenzar">COMENZAR</button>`;
     const btnComenzar = document.querySelector('#botonComenzar');
     btnComenzar.addEventListener('click', comenzar);
 }
 
 function comenzar() {
     contenedor.innerHTML = `<img class="carta" src="images/17.jpg">
-                            <button id="botonNuevaCarta">NUEVA CARTA</button>`;
+                            <button class="boton" id="botonPrimeraCarta">SACAR 1ª CARTA</button>`;
+    const btnPrimeraCarta = document.querySelector('#botonPrimeraCarta');
+    btnPrimeraCarta.addEventListener('click', nuevaCarta);
 }
+
+function nuevaCarta() {
+    let random = Math.floor(Math.random() * arrayCartas.length);
+    contenedor.innerHTML = `${arrayCartas[random]}
+                            <br>
+                            <button class="boton" id="botonNuevaCarta">NUEVA CARTA</button>
+                            `;
+    const btnNuevaCarta = document.querySelector('#botonNuevaCarta');
+    btnNuevaCarta.addEventListener('click', nuevaCarta);
+
+}
+
